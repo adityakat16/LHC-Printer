f1be9316693544d921fe4534e33e01d258a368fb
adityakat16 <adityapkatti@gmail.com>
2026-09-05 20:55:09 +0530

    committing:# Please enter the commit message for your changes. Lines starting

--- backend/printkiosk/settings.py
+++ backend/printkiosk/settings.py
@@ -45,6 +45,7 @@ STATIC_URL = '/static/'
 # CORS / CSRF settings for the React development server
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3001',
+    'http://localhost:3001/'
     'http://127.0.0.1:3001',
     'http://localhost:3000',
     'http://127.0.0.1:3000',
@@ -53,6 +54,7 @@ CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3001',
+    'http://localhost:3001/'
     'http://127.0.0.1:3001',
     'http://localhost:8000',
     'http://127.0.0.1:8000',
