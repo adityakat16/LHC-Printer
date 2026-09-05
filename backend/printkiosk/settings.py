cad7e4de473a0fe33e79d062b09403b81df5c2d5
adityakat16 <adityapkatti@gmail.com>
2026-09-05 21:04:12 +0530

    Update settings.py

--- backend/printkiosk/settings.py
+++ backend/printkiosk/settings.py
@@ -54,7 +54,7 @@ CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3001',
-    'http://localhost:3001/'
+    'http://localhost:3001/',
     'http://127.0.0.1:3001',
     'http://localhost:8000',
     'http://127.0.0.1:8000',
